7db32ba27c03a2043e4800d42a71135f4dd92a2d
vianneychin <vianneyjchin@gmail.com>
2024-05-05 20:13:17 -0700

    chore: test auto deploy for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -263,7 +263,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            hi Full Stack Software Engineer, coffee addict, gym rat, and gamer.
+            Full Stack Software Engineer, coffee addict, gym rat, and gamer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             <span className="mr-2">👋</span>
